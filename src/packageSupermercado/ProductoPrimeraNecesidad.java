711f13425a23f1f214ef8ede95fb7da244aa6260
nikcolastancredi <nikco_tancredi@hotmail.com>
2019-09-07 14:01:46 -0300

    Ejercicio 2 - POO JAVA

--- src/packageSupermercado/ProductoPrimeraNecesidad.java
+++ src/packageSupermercado/ProductoPrimeraNecesidad.java
@@ -1,14 +1,23 @@
 package packageSupermercado;
 
 public class ProductoPrimeraNecesidad extends Producto {
-
+	
+	private Integer porcentajeDescuento;
+	
 	public ProductoPrimeraNecesidad(String nombre, Double precio, Boolean esCuidado) {
 		super(nombre, precio, esCuidado);
+		this.porcentajeDescuento = 10;
+	}
+	
+	
+	public ProductoPrimeraNecesidad(String nombre, Double precio, Boolean esCuidado, Integer porcentajeDescuento) {
+		super(nombre, precio, esCuidado);
+		this.porcentajeDescuento = porcentajeDescuento;
 	}
 
 	@Override
 	public Double getPrecio() {
-		return this.precio * 0.9d;
+		return (double) this.precio - (this.precio * porcentajeDescuento / 100);
 	}
 	
 	
--- src/packageSupermercadoTest/ProductoPrimeraNecesidadTest.java
+++ src/packageSupermercadoTest/ProductoPrimeraNecesidadTest.java
@@ -10,15 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@BeforeEach
 	public void setUp() {
-		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		leche = new ProductoPrimeraNecesidad("Leche", 8d, false,11);
+		arroz = new ProductoPrimeraNecesidad("Arroz", 18d, true,8);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
-		
-		assertEquals(7.2 , leche.getPrecio());
+		assertEquals(7.12 , leche.getPrecio());
+		assertEquals(16.56d, arroz.getPrecio());
 	}
 }
